d294353a53c196a07795c5697d39c0aaffd9c900
Daniel <djperrefort@gmail.com>
2023-08-15 14:19:44 -0400

    Adds version option to CLI

---
+++ lmod_ingest/__init__.py
@@ -0,0 +1,9 @@
+"""A simple command line tool for ingesting Lmod log data into a MySQL database."""
+
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version('quota-notifier')
+
+except importlib.metadata.PackageNotFoundError:  # pragma: no cover
+    __version__ = '0.0.0'
