7591ab5e5f953ed786d7eaf704db92db21031268
Daniel Perrefort <djperrefort@gmail.com>
2023-09-07 13:49:39 -0400

    Mental dump of test outlines

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,13 +1,15 @@
 """Tests for the ``utils`` module"""
 
+import sqlite3
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 from unittest import TestCase
 from unittest.mock import patch
 
 import pandas as pd
+import sqlalchemy as sa
 
-from lmod_ingest.utils import fetch_db_url, parse_log_data
+from lmod_ingest.utils import fetch_db_url, parse_log_data, ingest_data_to_db
 
 
 class TestFetchDBUrl(TestCase):
@@ -24,7 +26,7 @@ class TestFetchDBUrl(TestCase):
         """Test the returned URI is valid and matches environmental variables"""
 
         expected_url = 'postgresql+asyncpg://testuser:testpass@/testdb?host=testhost&port=5433'
-        self.assertEqual(fetch_db_url(), expected_url)
+        self.assertEqual(expected_url, fetch_db_url())
 
     @patch('os.getenv', side_effect=lambda x, default=None: {
         'DB_USER': 'testuser',
@@ -36,7 +38,7 @@ class TestFetchDBUrl(TestCase):
 
         db_url = fetch_db_url()
         expected_url = 'postgresql+asyncpg://testuser:testpass@/testdb?host=localhost&port=5432'
-        self.assertEqual(db_url, expected_url)
+        self.assertEqual(expected_url, db_url)
 
     def test_fetch_db_url_missing_user(self):
         """Test an error is raised when environmental variables are not specified"""
@@ -91,4 +93,67 @@ class ParseLogData(TestCase):
             'logname': [str(self.test_path), str(self.test_path)]
         })
 
-        pd.testing.assert_frame_equal(result_df, expected_df)
+        pd.testing.assert_frame_equal(expected_df, result_df)
+
+
+class TestIngestDataToDB(TestCase):
+    """Test the ``ingest_data_to_db`` function"""
+
+    def setUp(self):
+        """Create a temporary test database in memory"""
+
+        # Create an in-memory SQLite database
+        self.db_connection = sqlite3.connect(':memory:')
+        self.engine = sa.create_engine('sqlite://', echo=True)
+        self.metadata = sa.MetaData()
+
+        # Define a test table and create it in the database
+        self.table_name = 'test_table'
+        self.table = sa.Table(
+            self.table_name,
+            self.metadata,
+            sa.Column('id', sa.Integer, primary_key=True),
+            sa.Column('name', sa.String),
+        )
+
+        self.metadata.create_all(bind=self.engine)
+
+    def tearDown(self):
+        """Close any open database connections"""
+
+        self.db_connection.close()
+
+    def test_ingest_data_to_db(self):
+        # Define test data
+        data = pd.DataFrame({
+            'id': [1, 2, 3],
+            'name': ['Alice', 'Bob', 'Charlie']
+        })
+
+        # Call the function to ingest data into the test table
+        with self.engine.connect() as conn:
+            ingest_data_to_db(data, self.table_name, conn)
+
+        # Retrieve data from the database
+        with self.engine.connect() as conn:
+            rows = conn.execute(sa.select(self.table)).fetchall()
+            self.assertEqual(len(rows), len(data))
+
+            for i, row in enumerate(rows):
+                self.assertEqual(row['id'], data['id'][i])
+                self.assertEqual(row['name'], data['name'][i])
+
+    def test_ingest_data_to_db_empty_data(self):
+        # Define an empty DataFrame
+        data = pd.DataFrame()
+
+        # Call the function to ingest empty data
+        ingest_data_to_db(data, self.table_name, self.db_connection)
+
+        # Retrieve data from the database
+        with self.engine.connect() as conn:
+            result = conn.execute(sa.select([self.table]))
+
+            # Verify that the table is empty
+            rows = result.fetchall()
+            self.assertEqual(len(rows), 0)
