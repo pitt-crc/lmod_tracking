ed75b8f8726df4f30da78bed04ba67583176e68a
Daniel Perrefort <djperrefort@gmail.com>
2023-09-07 11:24:58 -0400

    Adds placeholder tests

---
+++ tests/test_placeholder.py
@@ -0,0 +1,10 @@
+"""This is a placeholder"""
+
+from unittest import TestCase
+
+
+class Test(TestCase):
+    """This is a placeholder"""
+
+    def test_something(self) -> None:
+        """This test will always pass"""
